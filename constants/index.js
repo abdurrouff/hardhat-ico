7d6c72734d4e0130a84bc6591a43d20b7351c880
Rawman <rouffaderemi@gmail.com>
2023-06-20 14:11:55 +0100

    initial commit

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  '0x2e9121B0C6c4D19Be20F4f4C294FefCD9338fdEA';
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
